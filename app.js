3df87cb9c683543b85eaf01081e8d368edd0b9e4
Ben Stein <bumstain@gmail.com>
2014-09-15 23:50:42 +0100

    Basic Instagram functionality

--- app.js
+++ app.js
@@ -1,20 +1,80 @@
 $(document).ready(function(){
 
   // Client information and API keys
-  var instagram = {
-    clientID: 'x',
-    apiHost: 'https://api.instagram.com',
-  }
+  var min='', 
+      url='',
+      instagram = {
+        clientID: '2a04d812bc124b2eb9f86bd1d883540a',
+        apiHost: 'https://api.instagram.com',
+      }
 
-  function imageLoad(){
+  // Load images from Instagram
+  function imageLoad() {                      
+    tag = $('input').val();
+
+    // AJAX request and callback
     $.ajax({
       type: "GET",
       dataType: "jsonp",
       cache: false,
-      url: instagram.apiHost + "/v1/tags/" + tag + "/media/recent/?client_id=" + instagram.clientID + "&count=12",
-      data: {'client_id': instagram.clientID, 'max_tag_id': min,},
-      success: function(image){
-      }
-    })
+      url: instagram.apiHost + "/v1/tags/" + tag + "/media/recent/?client_id=" + instagram.clientID + "&count=48",
+      data: {
+        'client_id': instagram.clientID, 
+        'max_tag_id': min
+      },
+
+      //Return image output
+      success: function(pic) {
+        min = pic.pagination.next_max_tag_id;
+        url = pic.pagination.next_url;
+        $('.controlbutton').removeClass('hidden');
+        console.dir(pic);
+        for (var i = 0; i < pic.data.length; i++) {
+          likes = pic.data[i].likes.count;
+          console.log(likes);
+          link = pic.data[i].link;
+          urlsrc = pic.data[i].images.thumbnail.url;
+          $("#output").append("<div id='outputpic'><a target='_blank' href='" + link + "'><div id='heartdiv'><div id='likesdiv'>" + likes + "</div></div><img src='" + urlsrc + "'></img></div>");
+        }  
+      }      
+    });
   }
+    
+    // Clear pictures and start new search
+    $('#clearall').on('click', function(){         
+        $('#output').empty();
+        $('.controlbutton').addClass('hidden');
+        $('input').val('');
+        $('input').focus();
+     })
+
+    // Clear input value
+    $('input').on('click focusin', function() {      
+        this.value = '';
+    });
+    
+    // Submit input after delay
+    var timerid;                                     
+    $('input').keyup(function(){
+      clearTimeout(timerid);
+      timerid = setTimeout(function(){
+        imageLoad(); 
+      }, 
+      300);
+    });
 });
+
+
+
+
+
+
+
+
+
+
+// POTENTIAL FURTHER FUNCTIONALITY: 
+//Load more images 
+     // $('#morepics').on('click', function() {         
+     //    loadInstagrams();
+     // })
